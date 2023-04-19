0b45a0f20db42600e2409735daeec7dab2c16daa
nikojpapa <npapa1994@gmail.com>
2023-04-19 10:59:28 -0600

    print account address

--- deploy/4_deploy_bounty_fallback_account.ts
+++ deploy/4_deploy_bounty_fallback_account.ts
@@ -1,16 +1,14 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types'
 import { DeployFunction } from 'hardhat-deploy/types'
-import { Create2Factory } from '../src/Create2Factory'
 import { ethers } from 'hardhat'
 import { createAccountOwnerLamport } from '../test/bounty-fallback-account/testutils-lamport'
 import { randomBytes } from 'crypto'
-import { BountyFallbackAccount__factory, BountyFallbackAccountFactory__factory } from '../typechain'
 
 const deployBountyFallbackAccount: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const provider = ethers.provider
   const from = await provider.getSigner().getAddress()
 
-  const accountOwner = createAccountOwnerLamport(64, 64)
+  const accountOwner = createAccountOwnerLamport(32, 32)
   const salt = randomBytes(16)
 
   const entrypoint = await hre.deployments.get('EntryPoint')
@@ -25,8 +23,13 @@ const deployBountyFallbackAccount: DeployFunction = async function (hre: Hardhat
   const factory = await ethers.getContractAt('BountyFallbackAccountFactory', factoryDeployment.address)
   console.log('==BountyFallbackAccountFactory addr=', factory.address)
 
-  const account = await factory.createAccount(accountOwner.baseWallet.address, salt, accountOwner.lamportKeys.publicKeys, signatureBounty.address)
-  console.log('==BountyFallbackAccount addr=', account)
+  const addressParams = [accountOwner.baseWallet.address, salt, accountOwner.lamportKeys.publicKeys, signatureBounty.address]
+  // @ts-ignore
+  const account = await factory.createAccount(...addressParams)
+  await account.wait()
+  // @ts-ignore
+  const accountAddress = await factory.getAddress(...addressParams)
+  console.log('==BountyFallbackAccount addr=', accountAddress)
 }
 
 module.exports = deployBountyFallbackAccount
