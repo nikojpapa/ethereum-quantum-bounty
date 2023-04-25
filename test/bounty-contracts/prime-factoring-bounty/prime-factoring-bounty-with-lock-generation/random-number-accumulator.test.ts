bb72b17a059303e400200d4f0d9dbd0e1f6142f3
nikojpapa <npapa1994@gmail.com>
2023-04-24 21:46:07 -0600

    slice off extra bits test

--- test/bounty-contracts/prime-factoring-bounty/prime-factoring-bounty-with-lock-generation/random-number-accumulator.test.ts
+++ test/bounty-contracts/prime-factoring-bounty/prime-factoring-bounty-with-lock-generation/random-number-accumulator.test.ts
@@ -7,15 +7,11 @@ import { arrayify } from 'ethers/lib/utils'
 describe('RandomNumberAccumulator', () => {
   const BYTES_PER_uint256 = 32
   const BITS_PER_BYTE = 8
-  const MAX_GAS_LIMIT = BigNumber.from('0xffffffffffffffff')
 
   const ethersSigner = ethers.provider.getSigner()
   let randomNumberAccumulator: RandomNumberAccumulator
 
   const _256BitPrime = BigNumber.from(arrayify('0xc66f06e1b45c9c55073ed83708f390c86fd13e874d211d405abe0d293682ff03'))
-  const _256BitPrimeWithoutLeadingBit = _256BitPrime.mask(255)
-  // const _384BitPrime = BigNumber.from(arrayify('0xe63c9c85d3ef302557ec69dd7bb313cd03c39fa2ac6c261120a858d1ded315036be457ff7ebb7624038fb3eb6bed5c7d'))
-  const _512BitPrime = BigNumber.from(arrayify('0xdf122aa1a14be816462ac30f4074c042e899276cfdf4f1c1943ba244edbc904a03faf637e7d554021160496e96dc35afc16758473036077af0ecda7290509a89'))
 
   it('should not finish if the first random number is prime, but there are not enough bits', async () => {
     const numberOfLocks = 1
@@ -33,6 +29,7 @@ describe('RandomNumberAccumulator', () => {
     const bytesPerPrime = BYTES_PER_uint256
     randomNumberAccumulator = await new RandomNumberAccumulator__factory(ethersSigner).deploy(numberOfLocks, primesPerLock, bytesPerPrime)
 
+    const _256BitPrimeWithoutLeadingBit = _256BitPrime.mask(255)
     await randomNumberAccumulator.accumulate(_256BitPrimeWithoutLeadingBit)
     expect(await randomNumberAccumulator.isDone()).to.be.eq(true)
   })
@@ -47,6 +44,7 @@ describe('RandomNumberAccumulator', () => {
     const bytesPerPrime = BYTES_PER_uint256 * 2
     randomNumberAccumulator = await new RandomNumberAccumulator__factory(ethersSigner).deploy(numberOfLocks, primesPerLock, bytesPerPrime)
 
+    const _512BitPrime = BigNumber.from(arrayify('0xdf122aa1a14be816462ac30f4074c042e899276cfdf4f1c1943ba244edbc904a03faf637e7d554021160496e96dc35afc16758473036077af0ecda7290509a89'))
     const firstHalf = _512BitPrime.shr(BYTES_PER_uint256 * BITS_PER_BYTE)
     const secondHalf = _512BitPrime.mask(BYTES_PER_uint256 * BITS_PER_BYTE)
     await randomNumberAccumulator.accumulate(firstHalf)
@@ -56,6 +54,15 @@ describe('RandomNumberAccumulator', () => {
   })
 
   it('should slice off extra bits', async () => {
-    expect.fail()
+    const numberOfLocks = 1
+    const primesPerLock = 1
+    const bytesPerPrime = 1
+    randomNumberAccumulator = await new RandomNumberAccumulator__factory(ethersSigner).deploy(numberOfLocks, primesPerLock, bytesPerPrime)
+
+    const oneBytePrime = 0xbf
+    const remainingBits = (BYTES_PER_uint256 - bytesPerPrime) * BITS_PER_BYTE
+    const primeWithAdditionalBitsThatMakeItComposite = BigNumber.from(oneBytePrime).shl(remainingBits)
+    await randomNumberAccumulator.accumulate(primeWithAdditionalBitsThatMakeItComposite)
+    expect(await randomNumberAccumulator.isDone()).to.be.eq(true)
   })
 })
