9ead70f4f777f7e067af7f0c06b8a94fe1c420ad
nikojpapa <npapa1994@gmail.com>
2023-07-03 10:17:18 -0600

    multiple deployments for max gas

--- deploy/5_deploy_prime_factoring_bounty.ts
+++ deploy/5_deploy_prime_factoring_bounty.ts
@@ -7,32 +7,39 @@ import { BigNumber } from 'ethers'
 const MAX_GAS_LIMIT_OPTION = { gasLimit: BigNumber.from('0x1c9c380') }
 
 const deployPrimeFactoringBounty: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const provider = ethers.provider
-  const from = await provider.getSigner().getAddress()
-  await new Create2Factory(ethers.provider).deployFactory()
+  const gasUseds = []
+  let maxGasUsed = BigNumber.from(0)
+  for (let i = 0; i < 10; i++) {
+    const provider = ethers.provider
+    const from = await provider.getSigner().getAddress()
+    await new Create2Factory(ethers.provider).deployFactory()
 
-  const numberOfLocks = 120
-  const primeByteLength = 128
-  let gasUsed = BigNumber.from(0)
+    const numberOfLocks = 120
+    const primeByteLength = 128
+    let gasUsed = BigNumber.from(0)
 
-  const deployResult = await hre.deployments.deploy(
-    'PrimeFactoringBountyWithRsaUfo', {
-      ...MAX_GAS_LIMIT_OPTION,
-      from,
-      args: [numberOfLocks, primeByteLength],
-      gasLimit: 6e6,
-      deterministicDeployment: true
-    })
-  console.log('==PrimeFactoringBounty addr=', deployResult.address)
-  gasUsed = gasUsed.add(deployResult.receipt?.gasUsed)
+    const deployResult = await hre.deployments.deploy(
+      'PrimeFactoringBountyWithRsaUfo', {
+        ...MAX_GAS_LIMIT_OPTION,
+        from,
+        args: [numberOfLocks, primeByteLength],
+        gasLimit: 6e6,
+        deterministicDeployment: true
+      })
+    console.log('==PrimeFactoringBounty addr=', deployResult.address)
+    gasUsed = gasUsed.add(deployResult.receipt?.gasUsed)
 
-  const bounty = await ethers.getContractAt('PrimeFactoringBountyWithRsaUfo', deployResult.address)
-  while (!(await bounty.generationIsDone())) {
-    const tx = await bounty.triggerLockAccumulation()
-    const receipt = await tx.wait()
-    gasUsed = gasUsed.add(receipt.gasUsed)
+    const bounty = await ethers.getContractAt('PrimeFactoringBountyWithRsaUfo', deployResult.address)
+    while (!(await bounty.generationIsDone())) {
+      const tx = await bounty.triggerLockAccumulation()
+      const receipt = await tx.wait()
+      gasUsed = gasUsed.add(receipt.gasUsed)
+    }
+    gasUseds.push(gasUsed)
+    if (gasUsed.gt(maxGasUsed)) maxGasUsed = gasUsed
+    console.log('==PrimeFactoringBounty gasUsed this iteration=', gasUsed.toHexString())
+    console.log('==PrimeFactoringBounty max gasUsed=', maxGasUsed.toHexString())
   }
-  console.log('==PrimeFactoringBounty gasUsed=', gasUsed.toHexString())
 }
 
 module.exports = deployPrimeFactoringBounty
