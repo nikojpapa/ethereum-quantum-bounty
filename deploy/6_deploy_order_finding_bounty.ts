d27f6822808efb9f9ed3972e7cc9ce8443ecbff2
nikojpapa <npapa1994@gmail.com>
2023-08-14 11:16:24 -0600

    print number of accumulations in order finding deploy

--- deploy/6_deploy_order_finding_bounty.ts
+++ deploy/6_deploy_order_finding_bounty.ts
@@ -6,14 +6,15 @@ import { BigNumber } from 'ethers'
 
 const MAX_GAS_LIMIT_OPTION = { gasLimit: BigNumber.from('0x1c9c380') }
 
-const deployPrimeFactoringBounty: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+const deployOrderFindingBounty: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const provider = ethers.provider
   const from = await provider.getSigner().getAddress()
   await new Create2Factory(ethers.provider).deployFactory()
 
-  const numberOfLocks = 23
+  const numberOfLocks = 1
   const byteSizeOfModulus = 98
   let gasUsed = BigNumber.from(0)
+  let numberOfAccumulations = 0
 
   const deployResult = await hre.deployments.deploy(
     'OrderFindingBountyWithLockGeneration', {
@@ -23,21 +24,23 @@ const deployPrimeFactoringBounty: DeployFunction = async function (hre: HardhatR
       gasLimit: 6e6,
       deterministicDeployment: true
     })
-  console.log('==PrimeFactoringBounty addr=', deployResult.address)
+  console.log('==OrderFindingBounty addr=', deployResult.address)
   gasUsed = gasUsed.add(deployResult.receipt?.gasUsed)
 
   const bounty = await ethers.getContractAt('OrderFindingBountyWithLockGeneration', deployResult.address)
   while (!(await bounty.generationIsDone())) {
+    ++numberOfAccumulations
     const tx = await bounty.triggerLockAccumulation(MAX_GAS_LIMIT_OPTION)
     const receipt = await tx.wait()
     gasUsed = gasUsed.add(receipt.gasUsed)
   }
-  console.log('==PrimeFactoringBounty gasUsed=', gasUsed.toHexString())
+  console.log('==OrderFindingBounty gasUsed=', gasUsed.toHexString())
   const modulus = await bounty.locks(0, 0)
   const base = await bounty.locks(0, 1)
   console.log('Modulus: ', modulus)
   console.log('Base: ', base)
+  console.log(`Number of accumulations: ${numberOfAccumulations}`)
 }
 
-module.exports = deployPrimeFactoringBounty
-module.exports.tags = ['PrimeFactoringBounty']
+module.exports = deployOrderFindingBounty
+module.exports.tags = ['OrderFindingBounty']
